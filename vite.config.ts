99e5bf8ad92e46127b50e6887fd118729cea0b8e
Ganer03 <ivimiamia@gmail.com>
2024-05-30 19:11:51 +0300

    Create

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api/": {
-        target: "http://fktpm.k-lab.su",
+        target: "https://fktpm.k-lab.su",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/api/, '')
